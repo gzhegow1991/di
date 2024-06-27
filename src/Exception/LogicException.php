744c828d57edc1f8b2e73b7ade73427ac62c785e
gzhegow <6562680@gmail.com>
2024-06-27 17:38:10 +0300

    main

--- src/Exception/LogicException.php
+++ src/Exception/LogicException.php
@@ -2,7 +2,6 @@
 
 namespace Gzhegow\Di\Exception;
 
-
 use Gzhegow\Di\Lib;
 
 
@@ -11,17 +10,14 @@ class LogicException extends \LogicException implements
 {
     public $message;
     public $code;
-    public $file;
-    public $line;
     public $previous;
-
-    public $messageData;
-    public $messageObject;
 
     public function __construct(...$errors)
     {
-        foreach ( Lib::php_throwable_args()(...$errors) as $k => $v ) {
-            $this->{$k} = $v;
+        foreach ( Lib::php_throwable_args(...$errors) as $k => $v ) {
+            if (property_exists($this, $k)) {
+                $this->{$k} = $v;
+            }
         }
 
         parent::__construct($this->message, $this->code, $this->previous);
